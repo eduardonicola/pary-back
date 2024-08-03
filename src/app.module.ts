16808890fa904f3394cc69b43f9d6001dd7311ed
eduardonicola <eduardo.possani@greenn.com.br>
2024-08-03 15:22:37 -0300

    improvement: prism structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//Model do Database
-import { TypeOrmModule } from '@nestjs/typeorm';
-//Model da aplicação
-import { UsersModule } from './users/users.module';
-
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    UsersModule
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
