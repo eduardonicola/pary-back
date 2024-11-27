869557109411d25d446e37dccc1790dd636eb4f5
eduardonicola <possaniduardo@gmail.com>
2024-11-27 16:23:40 -0300

    feat sub

--- src/subscription/subscription.controller.ts
+++ src/subscription/subscription.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post, UseGuards } from '@nestjs/common';
+import { User } from 'src/auth/get-user.decorator';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { SubscriptionService } from './subscription.service';
 
 @Controller('subscription')
-export class SubscriptionController {}
+export class SubscriptionController {
+  constructor(private readonly subscriptionService: SubscriptionService ){} 
+
+  @Post(":uuid_event")
+  @UseGuards(JwtAuthGuard)
+  subscrib(@Param('uuid_event') uuid_event: string, @User('uuid_user') userId: string){
+    return this.subscriptionService.creatSub(uuid_event, userId)
+  }
+}
--- src/subscription/subscription.service.ts
+++ src/subscription/subscription.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { log } from 'console';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class SubscriptionService {}
+export class SubscriptionService {
+  constructor( private readonly prisma: PrismaService) {}
+
+  async creatSub(uuid_event: string, uuid_user: string) {
+    const event = await this.prisma.event.findUnique({
+      where: { uuid_event: uuid_event },
+      include:{
+        userHasevent: true
+      }
+    })  
+    if (!event) {
+      throw new BadRequestException('Evento não encontrado');
+    }
+    const currentDate = new Date();
+
+    // Verificando se a data de término das inscrições já passou
+    if (new Date(event.date_stop_sub) < currentDate) {
+      throw new BadRequestException('O período de inscrições já terminou');
+    }
+    // Verificando se o usuário já está inscrito
+    const isUserAlreadyEnrolled = event.userHasevent.some(
+      (userEvent) => userEvent.uuid_user === uuid_user, // Verifica se o uuid_user já está na lista
+    );
+
+    if (isUserAlreadyEnrolled) {
+      throw new BadRequestException('Usuário já está inscrito neste evento');
+    }
+
+    try {
+     const uhe = this.prisma.userHasEvent.create({
+      
+        data:{
+          uuid_event: uuid_event,
+          uuid_user: uuid_user,
+          user_level: "guest",
+        }
+      })
+      return uhe
+    } catch (error) {
+      return error
+    }
+
+  }
+}
